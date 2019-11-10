50e648d7c12bd6ec1e572767f968192158c2a862
pt <2286931835@qq.com>
2019-11-10 21:56:28 +0800

    pt

---
+++ hello=springcloud-web-admin-feign/src/main/java/com/pt/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
@@ -0,0 +1,12 @@
+package com.pt.hello.spring.cloud.web.admin.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminFeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminFeignApplication.class,args);
+
+    }
+}
